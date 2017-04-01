4479806924609e6e686125cf0c5df4c201efda91
Kilik Kuo <kikuo@mozilla.com>
2017-04-01 14:20:39 +0800

    Add ASCII color code for different information.

--- logger.py
+++ logger.py
@@ -1,44 +1,51 @@
-MSG_INFO    = 0x01
-MSG_WARNING = 0x02
-MSG_ERROR   = 0x04
-MSG_VERBOSE = 0x08
-MSG_ALL     = MSG_INFO | MSG_WARNING | MSG_ERROR | MSG_VERBOSE
-
 def logi(msg):
-    print("[INFO] " + msg)
+    print('[INFO] ' + msg + '\033[m')
 def logv(msg):
-    print("[VERBOSE] " + msg)
+    # Light-blue
+    print('\033[1;34m[VERBOSE] ' + msg + '\033[m')
 def logw(msg):
-    print("[WARNING] " + msg)
+    # Light-Cyan
+    print('\033[1;36m[WARNING] ' + msg + '\033[m')
 def loge(msg):
-    print("[ERROR] " + msg)
+    # Light-red
+    print('\033[1;31m[ERROR] ' + msg + '\033[m')
 
 class Logger(object):
+    MSG_INFO    = 0x01
+    MSG_WARNING = 0x02
+    MSG_ERROR   = 0x04
+    MSG_VERBOSE = 0x08
+    MSG_ALL     = MSG_INFO | MSG_WARNING | MSG_ERROR | MSG_VERBOSE
     def __init__(self):
         # TODO : Ability to change logger_level during runtime.
         #       i.e. On Windows, read registry key
         #            On Unix-like OSs, read/write to a specific file object.
-        self.logger_level = MSG_ALL
+        self.logger_level = Logger.MSG_ALL
+
     def info(self, msg):
-        if self.logger_level & MSG_INFO:
+        if self.logger_level & Logger.MSG_INFO:
             logi(msg)
+
     def warning(self, msg):
-        if self.logger_level & MSG_WARNING:
+        if self.logger_level & Logger.MSG_WARNING:
             logw(msg)
+
     def error(self, msg):
-        if self.logger_level & MSG_ERROR:
+        if self.logger_level & Logger.MSG_ERROR:
             loge(msg)
+
     def verbose(self, msg):
-        if self.logger_level & MSG_VERBOSE:
+        if self.logger_level & Logger.MSG_VERBOSE:
             logv(msg)
+
     def set_level(self, level):
         self.logger_level = level
 
 if __name__ == "__main__":
     logger = Logger()
     logger.verbose(" Test !")
-    print(" Set level to MSG_WARNING only")
-    logger.set_level(MSG_WARNING)
+    print("Set level to MSG_WARNING only")
+    logger.set_level(Logger.MSG_WARNING)
     logger.info("Nothing should be displayed")
     logger.error("Nothing should be displayed")
     logger.verbose("Nothing should be displayed")
